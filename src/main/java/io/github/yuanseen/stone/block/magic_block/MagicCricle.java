47679062da6a8ff9849fb58a38cc0bb53787b70e
YuanSeen <YuanSeen@foxmail.com>
2024-11-02 02:04:36 +0800

    做了凋落物处理

--- src/main/java/io/github/yuanseen/stone/block/magic_block/MagicCricle.java
+++ src/main/java/io/github/yuanseen/stone/block/magic_block/MagicCricle.java
@@ -1,15 +1,20 @@
 package io.github.yuanseen.stone.block.magic_block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -19,10 +24,23 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
+import javax.annotation.Nullable;
+import java.util.UUID;
+
+import static net.minecraft.world.entity.Entity.RemovalReason.UNLOADED_TO_CHUNK;
+
 public class MagicCricle extends Block {
 
     public static final IntegerProperty MAGICKITEMID = IntegerProperty.create("magic_item_id",0,64);
     public static final VoxelShape SHAPE = Block.box(0,0,0,16,3,16);
+
+    private static BlockPos pos = new BlockPos(0, 0, 0);;
+    @Nullable
+    private UUID itemEntityUUID;
+
+    public void setItemEntityUUID(ItemEntity itemEntity) {
+        this.itemEntityUUID = itemEntity.getUUID();
+    }
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
@@ -44,16 +62,41 @@ public class MagicCricle extends Block {
             pLevel.setBlock(pPos,pState.setValue(MAGICKITEMID,id),2);
         if (id!=0){
             ItemEntity itemEntity = new ItemEntity(EntityType.ITEM,pLevel);
+            setItemEntityUUID(itemEntity);
             itemEntity.setItem(item.getDefaultInstance());
             itemEntity.setNeverPickUp();
             itemEntity.setPos(pPos.getX()+0.5,pPos.getY()+1,pPos.getZ()+0.5);
             pLevel.addFreshEntity(itemEntity);
             itemStack.shrink(1);
-            }
+            }else{
+            killItemEntity(this.itemEntityUUID,pLevel);
+        }
 
             return InteractionResult.sidedSuccess(pLevel.isClientSide);
 
     }
+
+    public void killItemEntity(UUID beKillTtemEntityUUID,Level pLevel){
+        if (beKillTtemEntityUUID != null) {
+            Level level = pLevel;
+            if (level instanceof ServerLevel serverlevel) {
+                serverlevel.getEntity(beKillTtemEntityUUID).remove(UNLOADED_TO_CHUNK);
+            }
+        }
+    }
+
+
+    @Override
+    public void destroy(LevelAccessor pLevel, BlockPos pPos, BlockState pState) {
+        killItemEntity(this.itemEntityUUID, (Level) pLevel);
+
+    }
+
+    @Override
+    public void wasExploded(Level pLevel, BlockPos pPos, Explosion pExplosion) {
+        killItemEntity(this.itemEntityUUID,pLevel);
+    }
+
 
     public MagicCricle(Properties p_49795_) {
         super(p_49795_);
---
+++ src/main/java/io/github/yuanseen/stone/mixin/MixinsItemEntityToBlock.java
@@ -0,0 +1,15 @@
+//package io.github.yuanseen.stone.mixin;
+//
+//import net.minecraft.server.MinecraftServer;
+//import net.minecraft.world.entity.item.ItemEntity;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//
+//@Mixin(ItemEntity.class)
+//public class MixinsItemEntityToBlock {
+//    @Inject(at = @At("HEAD"),method = "")
+//    private void init(CallbackInfo info){
+//    }
+//}
